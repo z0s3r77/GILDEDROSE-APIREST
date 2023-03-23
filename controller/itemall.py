9dfc74490014f1279988d85219bbe7434401a708
z0s3r77 <ipopdue24@gmail.com>
2023-03-23 10:54:12 +0100

    feature(*): Añadida nueva solicitud

--- controller/itemall.py
+++ controller/itemall.py
@@ -6,4 +6,8 @@ from service.service import getAllItems
 class ItemAll(Resource):
     def get(self):
         allItems = getAllItems()
+        if not allItems:
+            # 404 not found
+            return {"Error Message": "The items are not in the DB, intialize with /db/initialize"}, 404
+        # 200 ok
         return allItems, 200
